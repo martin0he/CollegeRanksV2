ebc0d17df4852d9aa7264e18ebb257623ef12e81
martin0he <martin.hema.mh@gmail.com>
2024-07-10 20:29:43 +0200

    navigation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import ReviewPage from "./pages/ReviewPage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#777C49", // Custom primary color
+    },
+    secondary: {
+      main: "#80727B", // Custom secondary color
+    },
+    error: {
+      main: "#ff1744",
+    },
+    background: {
+      default: "#F9ECEC",
+    },
+  },
+  typography: {
+    fontFamily: '"Jost", "Helvetica", "Arial", sans-serif',
+    fontSize: 16,
+
+    h1: {
+      fontSize: "2.2rem",
+    },
+    h2: {
+      fontSize: "1.8rem",
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <Navbar />
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/colleges" element={<DashboardPage />} />
-        <Route path="/colleges/:id" element={<ReviewPage />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/leaderboard" element={<DashboardPage />} />
+          <Route path="/review" element={<ReviewPage />} />
+          <Route path="/account" element={<ReviewPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,147 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import useWindowDimensions from "../useWindowDimensions";
+import { useState } from "react";
 
 const Navbar = () => {
+  const { width } = useWindowDimensions();
+  const isSmallScreen = width < 680;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const theme = useTheme();
+
   return (
-    <Box
-      height="70px"
-      sx={{
-        width: "100%",
-        boxShadow: "-1px 2px 3px #7d6868",
-        backgroundColor: "#f1dede",
-        position: "fixed",
-        top: 0,
-        left: 0,
-      }}
-    ></Box>
+    <>
+      <Drawer
+        anchor="right"
+        open={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+      >
+        <Box
+          sx={{
+            width: 250,
+            height: "100%",
+            backgroundColor: theme.palette.background.default,
+            boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.2)",
+          }}
+          role="presentation"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <Link href="/" sx={{ textDecoration: "none" }}>
+                  <ListItemText primary="home" />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            {["leaderboard", "review", "account"].map((text) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <Link href={`/${text}`} sx={{ textDecoration: "none" }}>
+                    <ListItemText primary={text} />
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+      {isSmallScreen ? (
+        <Box
+          display="flex"
+          height="70px"
+          sx={{
+            width: "100%",
+            position: "fixed",
+            top: 0,
+            right: 0,
+          }}
+        >
+          <IconButton
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            sx={{
+              position: "absolute",
+              right: "15px",
+              top: "15px",
+              fontsize: 28,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+      ) : (
+        <Box
+          height="70px"
+          marginTop="20px"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: "100%",
+            position: "fixed",
+            top: 0,
+            left: 0,
+          }}
+        >
+          <Link
+            href="/"
+            sx={{
+              display: "inline-block",
+              padding: "20px",
+              fontSize: "20px",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            <Typography fontSize={28}>home</Typography>
+          </Link>
+          <Link
+            href="/leaderboard"
+            sx={{
+              display: "inline-block",
+              padding: "20px",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            <Typography fontSize={28}>leaderboard</Typography>
+          </Link>
+          <Link
+            href="/review"
+            sx={{
+              display: "inline-block",
+              padding: "20px",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            <Typography fontSize={28}>review</Typography>
+          </Link>
+          <Link
+            href="/account"
+            sx={{
+              display: "inline-block",
+              padding: "20px",
+              fontWeight: "bold",
+              color: "black",
+            }}
+          >
+            <Typography fontSize={28}>account</Typography>
+          </Link>
+        </Box>
+      )}
+    </>
   );
 };
 
