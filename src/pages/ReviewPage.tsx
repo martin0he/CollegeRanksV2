42d342489c4675ebc4fbe39cdfa3d5e766997950
martin0he <martin.hema.mh@gmail.com>
2024-07-11 16:53:09 +0200

    adding button

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,11 +1,13 @@
 import {
   Autocomplete,
   Box,
+  Button,
   Grid,
   Slider,
   TextField,
   Tooltip,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import supabase from "../supabase";
@@ -26,6 +28,7 @@ const ReviewPage = () => {
   const [safety, setSafety] = useState<number>(50);
   const [overall, setOverall] = useState<number>(0);
 
+  const theme = useTheme();
   const { width } = useWindowDimensions();
 
   useEffect(() => {
@@ -69,18 +72,18 @@ const ReviewPage = () => {
 
   return (
     <Box width="90vw" height="75vh">
-      <Grid container padding="15px">
-        <Grid item md={6} sm={12} xs={12} padding="15px" margin="15px">
+      <Grid container margin="45px">
+        <Grid item md={6} sm={12} xs={12} padding="15px">
           <Autocomplete
             id="combo-box-demo"
             options={universities}
             getOptionLabel={(option: University) =>
               `${option.name}, ${option.countryCode}`
             }
-            onInputChange={(e, newInputValue) => {
+            onInputChange={(_e, newInputValue) => {
               setInputValue(newInputValue);
             }}
-            onChange={(e, newValue) => {
+            onChange={(_e, newValue) => {
               setUniversityId(newValue?.id);
               console.log(newValue?.id);
             }}
@@ -101,232 +104,301 @@ const ReviewPage = () => {
               />
             )}
           />
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="A measure of academic quality, faculty, and research opportunities."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+          <Box
+            maxHeight="64vh"
+            overflow={"auto"}
+            marginTop="20px"
+            sx={{
+              "::-webkit-scrollbar": {
+                width: "8px",
+                borderRadius: "10px",
+              },
+              "::-webkit-scrollbar-track": {
+                backgroundColor: "#bcb8b8",
+                borderRadius: "10px",
+              },
+              "::-webkit-scrollbar-thumb": {
+                backgroundColor: theme.palette.secondary.main,
+                borderRadius: "10px",
+                "&:hover": {
+                  backgroundColor: theme.palette.secondary.dark,
+                },
+              },
+            }}
+          >
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="A measure of academic quality, faculty, and research opportunities."
                 >
-                  Academics
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Academics
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={academics}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setAcademics(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={academics}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setAcademics(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="Relates to the quality of the dormitories and off-campus housing."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="Relates to the quality of the dormitories and off-campus housing."
                 >
-                  Housing
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Housing
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={housing}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setHousing(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={housing}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setHousing(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The quality and convenience of the surrounding area and the campus itself."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The quality and convenience of the surrounding area and the campus itself."
                 >
-                  Location
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Location
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={location}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setLocation(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={location}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setLocation(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The variety and quality of clubs and extracurricular activities."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The variety and quality of clubs and extracurricular activities."
                 >
-                  Clubs
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Clubs
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={clubs}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setClubs(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={clubs}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setClubs(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The quality, variety, sustainability, and inclusivity of food options on and around campus."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The quality, variety, sustainability, and inclusivity of food options on and around campus."
                 >
-                  Dining
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Dining
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={food}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setFood(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={food}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setFood(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The noteworthiness of social life and events on campus."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The noteworthiness of social life and events on campus."
                 >
-                  Social
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Social
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={social}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setSocial(newValue as number)}
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={social}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setSocial(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The availability and quality of internships, research, networking, and job opportunities."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The availability and quality of internships, research, networking, and job opportunities."
                 >
-                  Opportunities
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Opportunities
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={opportunities}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) =>
+                  setOpportunities(newValue as number)
+                }
+              />
             </Box>
-            <Slider
-              sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              }}
-              size="small"
-              value={opportunities}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setOpportunities(newValue as number)}
-            />
-          </Box>
 
-          <Box width="fit-content" height="fit-content" marginY={1.2}>
-            <Box display="inline-flex" alignItems="center">
-              <Tooltip
-                arrow
-                placement="right"
-                title="The safety and security of the campus and surrounding area."
-              >
-                <Typography
-                  fontWeight={500}
-                  fontSize={22}
-                  sx={{ color: "#484847" }}
+            <Box width="fit-content" height="fit-content" marginY={1.2}>
+              <Box display="inline-flex" alignItems="center">
+                <Tooltip
+                  arrow
+                  placement="right"
+                  title="The safety and security of the campus and surrounding area."
                 >
-                  Safety
-                </Typography>
-              </Tooltip>
+                  <Typography
+                    fontWeight={500}
+                    fontSize={22}
+                    sx={{ color: "#484847" }}
+                  >
+                    Safety
+                  </Typography>
+                </Tooltip>
+              </Box>
+              <Slider
+                sx={{
+                  width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                }}
+                size="small"
+                value={safety}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                onChange={(_e, newValue) => setSafety(newValue as number)}
+              />
             </Box>
-            <Slider
+
+            <Button
               sx={{
-                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                marginY: "10px",
+                textTransform: "none",
+                fontSize: "22px",
+                borderRadius: "8px",
+                padding: "5px 15px",
+                "&:hover": {
+                  backgroundColor: theme.palette.primary.main,
+                  color: theme.palette.background.default,
+                },
               }}
-              size="small"
-              value={safety}
-              aria-label="Small"
-              valueLabelDisplay="auto"
-              onChange={(e, newValue) => setSafety(newValue as number)}
-            />
+            >
+              submit
+            </Button>
           </Box>
         </Grid>
-        <Grid item md={6} sm={12} xs={12}>
-          {overall.toFixed(1)}
+        <Grid
+          item
+          md={6}
+          sm={12}
+          xs={12}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            alignItems: "flex-end",
+          }}
+        >
+          <Box display="flex" alignItems="center">
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="flex-end"
+              marginTop="18px"
+            >
+              <Typography fontSize={22} fontWeight={500}>
+                Overall
+              </Typography>
+              <Typography fontSize={22} fontWeight={500}>
+                Score
+              </Typography>
+            </Box>
+            <Typography fontSize={105} fontWeight={650} sx={{ ml: 1 }}>
+              {overall.toFixed(1)}
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
     </Box>
