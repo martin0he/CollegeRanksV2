93c724d99f2cbd6dc62241b7e7ac8b5705cc09f6
martin0he <martin.hema.mh@gmail.com>
2024-07-11 18:04:08 +0200

    initial chart

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -1,5 +1,76 @@
+import { Grid, Typography } from "@mui/material";
+import { ChartData, ChartOptions } from "chart.js";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
+
 const LeaderboardPage = () => {
-  return <></>;
+  const data: ChartData<"bar"> = {
+    labels: [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ],
+    datasets: [
+      {
+        label: "Top 10 Universities",
+        data: [65.4, 68, 71.2, 73, 75, 78.3, 80.5, 82.7, 85, 87.2],
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options: ChartOptions<"bar"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Top 10 Universities",
+      },
+    },
+  };
+
+  return (
+    <Grid container width="90vw" height="75vh" spacing={1} marginTop="25px">
+      <Grid
+        item
+        md={9}
+        sm={12}
+        xs={12}
+        style={{ height: "80%", padding: "10px", margin: "30px" }}
+      >
+        <Bar data={data} options={options} />
+      </Grid>
+      <Grid
+        item
+        md={2}
+        sm={12}
+        xs={12}
+        style={{ padding: "10px", margin: "30px 10px" }}
+      >
+        <Typography>test</Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LeaderboardPage;
