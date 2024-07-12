45ab67557bc0fbd4aa04d61ef753e218ae37f450
martin0he <martin.hema.mh@gmail.com>
2024-07-12 11:18:57 +0200

    homepage padding

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -51,69 +51,77 @@ const HomePage = () => {
       flexDirection={"column"}
       justifyContent="center"
       alignItems="center"
-      height="70vh"
+      height="fit-content"
       width="100vw"
     >
-      <Typography
-        fontWeight={550}
-        sx={{ fontSize: { md: 100, sm: 80, xs: 60 } }}
-        color={"primary"}
+      <Box
+        padding="20px"
+        display="flex"
+        flexDirection={"column"}
+        justifyContent="center"
+        alignItems="center"
       >
-        CollegeRanks
-      </Typography>
-      <Typography
-        sx={{ fontSize: { md: 32, sm: 28, xs: 24 } }}
-        fontWeight={500}
-        color="secondary"
-      >
-        <Typewriter
-          options={
-            {
-              strings: [
-                "Compare Schools",
-                "Review Your School",
-                "Find Your Dream School",
-                "Look At Rankings",
-              ],
-              autoStart: true,
-              loop: true,
-              delay: 75,
-              pauseFor: 2000,
-            } as Partial<Options>
-          }
-        />
-      </Typography>
-
-      <Autocomplete
-        id="combo-box-demo"
-        options={universities}
-        getOptionLabel={(option: University) =>
-          `${option.name}, ${option.countryCode}`
-        }
-        onInputChange={(_e, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        onChange={handleUniversitySelect}
-        renderInput={(params) => (
-          <TextField
-            sx={{
-              "& .MuiInputBase-root": {
-                borderRadius: "35px",
-                backgroundColor: "#F9F4F4",
-                height: "45px",
-                boxShadow: "-1px 2px 1px #7a7171",
-                fontSize: "18px",
-                padding: "15px",
-              },
-              width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
-              height: "fit-content",
-              marginY: 3,
-            }}
-            {...params}
-            placeholder="Select University"
+        <Typography
+          fontWeight={550}
+          sx={{ fontSize: { md: 100, sm: 80, xs: 60 } }}
+          color={"primary"}
+        >
+          CollegeRanks
+        </Typography>
+        <Typography
+          sx={{ fontSize: { md: 32, sm: 28, xs: 24 } }}
+          fontWeight={500}
+          color="secondary"
+        >
+          <Typewriter
+            options={
+              {
+                strings: [
+                  "Compare Schools",
+                  "Review Your School",
+                  "Find Your Dream School",
+                  "Look At Rankings",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                pauseFor: 2000,
+              } as Partial<Options>
+            }
           />
-        )}
-      />
+        </Typography>
+        <Autocomplete
+          id="combo-box-demo"
+          options={universities}
+          getOptionLabel={(option: University) =>
+            `${option.name}, ${option.countryCode}`
+          }
+          onInputChange={(_e, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          onChange={handleUniversitySelect}
+          renderInput={(params) => (
+            <TextField
+              sx={{
+                "& .MuiInputBase-root": {
+                  borderRadius: "35px",
+                  backgroundColor: "#F9F4F4",
+                  height: "45px",
+                  boxShadow: "-1px 2px 1px #7a7171",
+                  fontSize: "18px",
+                  padding: "15px",
+                },
+                width: { md: 0.4 * width, sm: 0.55 * width, xs: 0.7 * width },
+                height: "fit-content",
+                marginY: 3,
+              }}
+              {...params}
+              placeholder="Select University"
+            />
+          )}
+        />
+      </Box>
+
       <Box
         sx={{
           display: "flex",
